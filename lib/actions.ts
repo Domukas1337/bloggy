ffd208fa5c786d1577e0d1a55193248779dd129a
Domas <domukas1337@gmail.com>
2025-04-03 20:16:37 +0300

    fixed empty author name and added loading screen

--- lib/actions.ts
+++ lib/actions.ts
@@ -5,7 +5,7 @@ import { supabase } from "./supabase";
 export async function createBlog(formData) {
   const newBlog = {
     title: formData.get("title"),
-    author: formData.get("author"),
+    author: formData.get("author") || "Anonymous",
     blog: formData.get("blog").slice(0, 5000),
   };
   const { error } = await supabase.from("bloggy").insert([newBlog]);
