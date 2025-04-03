7565af901a8c813cab2634239888ff319047a10b
Domas <domukas1337@gmail.com>
2025-04-03 14:37:52 +0300

    navbar buttons hitboxes

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,25 +6,31 @@ export default function Navbar() {
   const pathname = usePathname();
 
   return (
-    <div className="flex justify-center mt-2 mb-4">
+    <div className="flex justify-center mt-2 mb-2 py-2">
       <ul className="flex flex-row gap-10 text-lg">
-        <li
-          className={
-            pathname === "/"
-              ? "text-blue-500 border p-2 px-4"
-              : `hover:text-blue-500 transition-colors border p-2 px-4`
-          }
-        >
-          <Link href="/">Home</Link>
+        <li>
+          <Link
+            href="/"
+            className={
+              pathname === "/"
+                ? "text-blue-500 border p-2 px-4"
+                : `hover:text-blue-500 transition-colors border p-2 px-4`
+            }
+          >
+            Home
+          </Link>
         </li>
-        <li
-          className={
-            pathname === "/posts"
-              ? "text-blue-500 border p-2 px-4"
-              : `hover:text-blue-500 transition-colors border p-2 px-4`
-          }
-        >
-          <Link href="/posts">Posts</Link>
+        <li>
+          <Link
+            href="/posts"
+            className={
+              pathname === "/posts"
+                ? "text-blue-500 border p-2 px-4"
+                : `hover:text-blue-500 transition-colors border p-2 px-4`
+            }
+          >
+            Posts
+          </Link>
         </li>
       </ul>
     </div>
