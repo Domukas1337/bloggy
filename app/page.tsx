abf4462dc3439fc66a210f288c1c76e6ee4965aa
Domas <domukas1337@gmail.com>
2025-05-10 21:38:27 +0300

    text size changes

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,13 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-center gap-7 fadein">
-      <h1 className="text-3xl md:text-6xl font-bold">Welcome to Bloggy</h1>
-      <p className="text-xl md:text-2xl">
+    <main className="flex flex-col items-center justify-center gap-4 md:gap-7 fadein">
+      <h1 className="text-4xl md:text-6xl font-bold">Welcome to Bloggy</h1>
+      <p className="text-lg md:text-2xl">
         Blog posts without a need to sign up.
       </p>
       <p className="text-lg md:text-xl text-green-500">
-        Create your first post
+        Create your first post!
       </p>
       <Link
         className="text-2xl bg-green-700 p-4 hover:bg-green-600 transition-colors"
